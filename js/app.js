e40dbe4a8e8bcbe19caff7a07c2b19f0d99d1e83
Moksleivis <you@example.com>
2020-10-23 14:58:20 +0300

    java pradzia

---
+++ js/app.js
@@ -0,0 +1,29 @@
+//alert("Man veikia");//komentaras
+/*
+Autorius: x
+ */
+
+
+//let items = ['item1', 10, true, {name:"Aivaras"}, 'penktadienis'];//array
+
+//let addItem = items.push("Super item");//prideda itema i masyva
+//items.pop();//pasalina paskutini mnasyvo itema
+
+let sveciai = prompt('iveskite kiek yra sveciu');
+    let name = prompt('iveskite sveciu vardus');
+    console.log("vardas" +name)
+for(let i=0; i<sveciai; i++)
+if (++i>sveciai){
+
+}else{
+    let name = prompt('iveskite sveciu vardus')
+        console.log("vardas" +name)
+    ;
+}
+
+
+
+
+
+//console.log("masyvo elemento reiksme"+ items[3]);
+
